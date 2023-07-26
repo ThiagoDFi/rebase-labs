94ac4a369ffa86b4f796ed5bc3c747887c2a5787
Thiago Dantas Firmino <thiagod.firmino@gmail.com>
2023-07-26 12:09:11 -0300

    Adição de codigo para o erro de CORS

--- server.rb
+++ server.rb
@@ -5,6 +5,19 @@ require 'json'
 require_relative 'database'
 require_relative 'consult_exams'
 
+before do
+  response.headers['Access-Control-Allow-Origin'] = '*'
+  response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+  response.headers['Access-Control-Allow-Headers'] = 'accept, authorization, origin, content-type'
+end
+
+options "*" do
+  response.headers["Allow"] = "GET, POST, OPTIONS"
+  response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, Origin"
+  response.headers["Access-Control-Allow-Origin"] = "*"
+  200
+end
+
 consult_exams = ConsultExams.new
 
 get '/tests' do
